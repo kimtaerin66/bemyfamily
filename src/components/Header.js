64cbf2ca72fb1617868f4017429ebfa9256539bc
kimtaerin66 <yeonjikim66@gmail.com>
2022-05-31 16:37:43 +0900

    first

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Gnb = styled.Nav`
+
+`;
+
+
+function Header() {
+  return (
+    <div >
+     Header
+    </div>
+  );
+}
+
+export default Header;
